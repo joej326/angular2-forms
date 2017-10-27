fa15f6661e976c4ca66951575eacf35565c6ac68
Joseph Joyce <joej326@gmail.com>
2017-10-27 13:47:23 -0700

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  submitted = false;
+
+  userData = {
+    name: '',
+    gender: '',
+    answer: '',
+    email: ''
+  }
+
+
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = [] = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(formData){
+    console.log(formData);
+    this.submitted = true;
+    this.userData.name = formData.value.userData.username;
+    this.userData.gender = formData.value.gender;
+    this.userData.email = formData.value.userData.email;
+    this.userData.answer = formData.value.questionAnswer;
+
+    formData.reset();
   }
 }
